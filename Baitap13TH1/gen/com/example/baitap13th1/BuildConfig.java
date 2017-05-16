26c4a0c27dea7a8717b9c210311b2df593cc811c
Minh Khang <tikhang96@gmail.com>
2017-05-16 10:56:25 +0700

    baitap

---
+++ Baitap13TH1/gen/com/example/baitap13th1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.baitap13th1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Baitap13TH1/src/com/example/baitap13th1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.baitap13th1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final String arr[]={"Sach","Vo","But","Cap","balo"};
+        ListView lv=(ListView) findViewById(R.id.lvperson);
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>
+        (this, android.R.layout.simple_list_item_1, arr);
+        lv.setAdapter(adapter);
+        final TextView txt=(TextView) findViewById(R.id.textView1);
+        lv.setOnItemClickListener
+        (
+	        new AdapterView.OnItemClickListener() 
+	        {
+	        	public void onItemClick(AdapterView<?> arg0,View arg1,int arg2,long arg3) 
+	        	{
+		        txt.setText("position :"+arg2+" ; value ="+arr[arg2]);
+		        }
+		    }
+        );
+        }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
